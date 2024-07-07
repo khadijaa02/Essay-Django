a71b0038743916b5faf5b51d745524acee81d436
khadijaa02 <120328072+khadijaa02@users.noreply.github.com>
2024-07-07 23:53:12 +0100

    essay: django crud with postgres

--- Crud-Postgres/try/settings.py
+++ Crud-Postgres/try/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'khadija',
+        'PASSWORD': '******',
         'HOST': '',
         'PORT': '5433'
     }
